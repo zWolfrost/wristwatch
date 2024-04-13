5b5562026b9b80fbb6dc68910a55fdf930c8bda2
zWolfrost <zwolfrost@gmail.com>
2024-04-13 20:44:20 +0200

    v1.0.1
    -specified version requirements
    -added readme

--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
 	name="wristwatch",
-	version="1.0.0",
+	author="zWolfrost",
+	version="1.0.1",
+	description="Yet another Python watcher for website updates.",
+	long_description=open("README.md").read(),
+	long_description_content_type="text/markdown",
 	packages=find_packages(),
 	install_requires=[
-		"beautifulsoup4",
-		"selenium",
-		"rookiepy",
+		"beautifulsoup4>=4.11.0",
+		"selenium>=4.19.0",
+		"rookiepy>=0.4.0"
 	],
 	entry_points={
 		"console_scripts": [
